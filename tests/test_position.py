b36660e6370c494dc5266f6c3a7b9014fedf87ba
Jayampathi Rajapaksage <rjsbsamarathunga@gmail.com>
2023-06-15 19:03:39 +0000

    fix auto test

--- tests/test_position.py
+++ tests/test_position.py
@@ -1,12 +1,10 @@
-import unittest 
+from unittest import TestCase
 from levelup.position import Position
 
-class TestPosition(unittest.TestCase):
+class TestPosition(TestCase):
     def test_init(self):
         x = 1
         y = 5
         testobj = Position(x, y)
         self.assertEqual(x, testobj.x)
         self.assertEqual(y, testobj.y)
-
-unittest.main()
