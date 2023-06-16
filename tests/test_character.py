8060cb7e49c455d45dffe6bf90aa83e51fccf212
Gene Wubben <wubbeng@gmail.com>
2023-06-16 14:44:17 +0000

    updated tests for character

--- tests/test_character.py
+++ tests/test_character.py
@@ -37,4 +37,81 @@ class TestCharacterInitWithName(TestCase):
         new_move_count = testobj.get_num_moves()
         self.assertEqual(expected_move_count, new_move_count)
 
-    # TODO: ^ s, e, w, invalid
+    
+    def test_move_south(self):
+        ARBITRARY_NAME = "MyName"
+        testobj = Character(ARBITRARY_NAME)
+        current_move_count = testobj.get_num_moves()
+
+        # test position
+        current_position = Position(3,4)
+        testobj.set_position(current_position)
+        direction = Direction("s")
+        expected_position = Position(3,3)
+        testobj.move(direction)
+        updated_position = testobj.get_position()
+        self.assertTrue(updated_position == expected_position)
+
+        # test move count
+        expected_move_count = current_move_count+1
+        new_move_count = testobj.get_num_moves()
+        self.assertEqual(expected_move_count, new_move_count)
+
+    def test_move_east(self):
+        ARBITRARY_NAME = "MyName"
+        testobj = Character(ARBITRARY_NAME)
+        current_move_count = testobj.get_num_moves()
+
+        # test position
+        current_position = Position(3,4)
+        testobj.set_position(current_position)
+        direction = Direction("e")
+        expected_position = Position(4,4)
+        testobj.move(direction)
+        updated_position = testobj.get_position()
+        self.assertTrue(updated_position == expected_position)
+
+        # test move count
+        expected_move_count = current_move_count+1
+        new_move_count = testobj.get_num_moves()
+        self.assertEqual(expected_move_count, new_move_count)
+
+    def test_move_west(self):
+        ARBITRARY_NAME = "MyName"
+        testobj = Character(ARBITRARY_NAME)
+        current_move_count = testobj.get_num_moves()
+
+        # test position
+        current_position = Position(3,4)
+        testobj.set_position(current_position)
+        direction = Direction("w")
+        expected_position = Position(2,4)
+        testobj.move(direction)
+        updated_position = testobj.get_position()
+        self.assertTrue(updated_position == expected_position)
+
+        # test move count
+        expected_move_count = current_move_count+1
+        new_move_count = testobj.get_num_moves()
+        self.assertEqual(expected_move_count, new_move_count)    
+  
+
+    def test_move_west_atWestBoundary(self):
+        ARBITRARY_NAME = "MyName"
+        testobj = Character(ARBITRARY_NAME)
+        current_move_count = testobj.get_num_moves()
+
+        # test position
+        current_position = Position(0,4)
+        testobj.set_position(current_position)
+        direction = Direction("w")
+        expected_position = current_position
+        testobj.move(direction)
+        updated_position = testobj.get_position()
+        self.assertTrue(updated_position == expected_position)
+
+        # test move count
+        expected_move_count = current_move_count+1
+        new_move_count = testobj.get_num_moves()
+        self.assertEqual(expected_move_count, new_move_count)    
+  
