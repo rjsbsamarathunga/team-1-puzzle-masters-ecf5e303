d82e049a48b8ab3a7b357452a2c77569ddba5550
Jayampathi Rajapaksage <rjsbsamarathunga@gmail.com>
2023-06-16 15:40:57 +0000

    controller

--- src/levelup/controller.py
+++ src/levelup/controller.py
@@ -26,34 +26,42 @@ class GameController:
 
     def __init__(self):
         self.status = GameStatus()
+        self.character = None
 
     def start_game(self):
-        pass
+        self.character.enter_map()
+        current_position = self.character.get_position()
+        self.status.current_position = (current_position.x, current_position.y)
 
     # Pre-implemented to demonstrate ATDD
     # TODO: Update this if it does not match your design (hint - it doesnt)
     def create_character(self, character_name: str) -> None:
         if character_name is not None and character_name != "":
             self.status.character_name = character_name
+            self.character = Character(character_name)
         else:
             self.status.character_name = DEFAULT_CHARACTER_NAME
+            self.character = Character(DEFAULT_CHARACTER_NAME)
 
     def move(self, direction: Direction) -> None:
         # TODO: Implement move - should call something on another class
         # TODO: Should probably also update the game results
-        pass
+        self.character.move(direction)
+        current_position = self.character.get_position()
+        self.status.current_position = (current_position.x, current_position.y)
+        self.status.move_count = self.character.get_num_moves()
 
     def set_character_position(self, xycoordinates: tuple) -> None:
         # TODO: IMPLEMENT THIS TO SET CHARACTERS CURRENT POSITION -- exists to be testable
-        pass
+        self.character.set_position(xycoordinates([0]), xycoordinates([1]))
 
     def set_current_move_count(self, move_count: int) -> None:
         # TODO: IMPLEMENT THIS TO SET CURRENT MOVE COUNT -- exists to be testable
-        pass
+        this.status.move_count = self.character.get_num_moves
 
     def get_total_positions(self) -> int:
         # TODO: IMPLEMENT THIS TO GET THE TOTAL POSITIONS FROM THE MAP - - exists to be
         # testable
-        return -10
+        return GameMap.get_total_positions()
 
     
--- tests/test_game_controller.py
+++ tests/test_game_controller.py
@@ -1,8 +1,14 @@
 from unittest import TestCase
 from levelup.controller import GameController
 
-class TestGameController(TestCase):
+class TestGameControllerInit(TestCase):
     def test_init(self):
         testObj = GameController()
         assert testObj.status != None
-        
+        # assert testObj.character == None
+
+# class TestGameControllerStartGame(TestCase):
+#     def test_init(self):
+#         testObj = GameController()
+#         testObj.create_character("Test") 
+#         testObj.start_game()
