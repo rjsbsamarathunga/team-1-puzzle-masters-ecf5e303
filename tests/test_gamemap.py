4be30355e515b4a5808f9bdd91528d9e20b4a261
Gene Wubben <wubbeng@gmail.com>
2023-06-16 15:25:00 +0000

    added gamemap test

--- tests/test_gamemap.py
+++ tests/test_gamemap.py
@@ -25,12 +25,26 @@ class TestGameMap(TestCase):
         self.assertFalse(GameMap.is_position_valid(position))
 
     def test_is_position_valid_greaterthanxend_false(self):
-        x = -11
+        x = 11
         y = 0
         position = Position(x,y)
         self.assertFalse(GameMap.is_position_valid(position))
 
     # TODO: ^ y
+    def test_is_position_valid_lessthanystart_false(self):
+        x = 0
+        y = -1
+        position = Position(x,y)
+        self.assertFalse(GameMap.is_position_valid(position))
+
+    def test_is_position_valid_greaterthanyend_false(self):
+        x = 0
+        y = 11
+        position = Position(x,y)
+        self.assertFalse(GameMap.is_position_valid(position))
+
+
+
 
     def test_calculate_position_n(self):
         direction = Direction("n")
@@ -39,7 +53,34 @@ class TestGameMap(TestCase):
         new_position = GameMap.calculate_position(current_position, direction)
         self.assertTrue(new_position == expected_position)
 
-    # TODO: ^ for s, e, w, invalid
+    def test_calculate_position_s(self):
+        direction = Direction("s")
+        current_position = Position(5,6)
+        expected_position = Position(5,5)
+        new_position = GameMap.calculate_position(current_position, direction)
+        self.assertTrue(new_position == expected_position)
+
+    def test_calculate_position_e(self):
+        direction = Direction("e")
+        current_position = Position(5,6)
+        expected_position = Position(6,6)
+        new_position = GameMap.calculate_position(current_position, direction)
+        self.assertTrue(new_position == expected_position)
+
+    def test_calculate_position_w(self):
+        direction = Direction("w")
+        current_position = Position(5,6)
+        expected_position = Position(4,6)
+        new_position = GameMap.calculate_position(current_position, direction)
+        self.assertTrue(new_position == expected_position)
+
+    def test_calculate_position_at_south_edge(self):
+        direction = Direction("s")
+        current_position = Position(0,0)
+        expected_position = Position(0,0)
+        new_position = GameMap.calculate_position(current_position, direction)
+        self.assertTrue(new_position == expected_position)
+
 
     def test_total_positions(self):
         total_positions=100
